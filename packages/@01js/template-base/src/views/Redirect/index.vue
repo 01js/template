823e515bf46541b8cceed50c19a6da4a6c782129
binperson <549346275@qq.com>
2020-01-10 16:13:30 +0800

    init

---
+++ packages/@01js/template-base/src/views/Redirect/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+
+export default class Index extends Vue {
+  created() {
+    const { params, query } = this.$route
+    const { path } = params
+    this.$router.replace({ path: '/' + path, query })
+  }
+
+  protected render(h:any) {
+    return h() // avoid warning message
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
